9e714da1ba6f5186f2bfeec048a8c04e2b55cdc7
sanemi07 <arjuntripathi1724@gmail.com>
2026-02-12 08:43:37 +0530

    feat: update App component to change title dynamically with button click

--- week6/learning/my-vite-app/src/App.jsx
+++ week6/learning/my-vite-app/src/App.jsx
@@ -4,11 +4,16 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [title, setTitle] = useState("hii")
+  function changetitle(){
+    const number=Math.random()
+    setTitle(number)
+  }
 
   return (
     <div>
-      <Header title={"hello"}></Header>
+      <button onClick={changetitle}>click me to change title</button>
+      <Header title={`my name is ${title}`}></Header>
       <Header title={"hii"}/>
     </div>
   )
