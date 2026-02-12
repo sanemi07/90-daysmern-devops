65f87743f6a783c4b2f55d24b599784b311ba607
sanemi07 <arjuntripathi1724@gmail.com>
2026-02-12 08:54:10 +0530

    feat: refactor App component to encapsulate title change functionality in HeaderWithButton

--- week6/learning/my-vite-app/src/App.jsx
+++ week6/learning/my-vite-app/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
@@ -12,9 +11,22 @@ function App() {
 
   return (
     <div>
-      <button onClick={changetitle}>click me to change title</button>
-      <Header title={`my name is ${title}`}></Header>
+      <HeaderWithButton/>
       <Header title={"hii"}/>
+    </div>
+  )
+}
+function HeaderWithButton(){
+  const [title, setTitle] = useState("hii")
+  function changetitle(){
+    const number=Math.random()
+    setTitle(number)
+  }
+  return (
+    <div>
+      <button onClick={changetitle}>click me to change title</button>
+             <Header title={`my name is ${title}`}></Header>
+
     </div>
   )
 }
