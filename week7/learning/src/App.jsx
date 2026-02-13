f675891f245820dd25d69e4fe4e0453132b95b9b
sanemi07 <arjuntripathi1724@gmail.com>
2026-02-13 10:22:54 +0530

    feat: initialize React project with Vite, add routing and basic components
    - Created package.json with dependencies for React, React Router, and Vite.
    - Added SVG assets for Vite and React logos.
    - Implemented App component with routing to Home and Dashboard components.
    - Created Home and Dashboard components with basic structure.
    - Added global styles in index.css and App.css for layout and design.
    - Configured Vite with a basic setup in vite.config.js.

---
+++ week7/learning/src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Home from './components/Home'
+import Dashboard from './components/Dashboard'
+
+
+
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/Dashboard' element={<Dashboard/>}/>
+
+   
+
+    </Routes>
+    
+    
+    </BrowserRouter>
+     
+    </>
+  )
+}
+
+export default App
