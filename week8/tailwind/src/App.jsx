7cebadedfd05333c43f55b25f69189e47a35bf99
sanemi07 <arjuntripathi1724@gmail.com>
2026-02-14 16:16:46 +0530

    feat: add Navbar component for navigation and layout structure

--- week8/tailwind/src/App.jsx
+++ week8/tailwind/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import ReveneuCard from './components/ReveneuCard'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <div className=' grid grid-cols-1 md:grid md:grid-cols-3 '>
-   <ReveneuCard title={"Amount Pending"} amount={"92,312.20"} orders={"13"}></ReveneuCard>
+    <div>
+      <Navbar></Navbar>
    </div>
+   
   )
 }
 
---
+++ week8/tailwind/src/components/Navbar.jsx
@@ -0,0 +1,83 @@
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center p-4 shadow-md w-full">
+
+      {/* Left Section */}
+      <div className="flex items-center gap-4">
+        <div className="font-semibold text-2xl">
+          Payouts
+        </div>
+
+        <div className="flex gap-1 items-center cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
+            />
+          </svg>
+          <p className="text-gray-700 font-light">
+            How it Works
+          </p>
+        </div>
+      </div>
+
+      {/* mid - Search */}
+      <div className="w-full max-w-sm min-w-[200px]">
+        <div className="relative flex items-center">
+          
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="absolute w-5 h-5 top-2.5 left-2.5 text-slate-600"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
+            />
+          </svg>
+
+          <input
+            className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md pl-10 pr-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+            placeholder="UI Kits, Dashboards..."
+          />
+
+          <button
+            className="rounded-md bg-slate-800 py-2 px-4 text-sm text-white shadow-md hover:bg-slate-700 ml-2"
+            type="button"
+          >
+            Search
+          </button>
+
+        </div>
+      </div>
+      {/*right Section*/}
+      <div className="flex gap-3 p-2">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+         <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M9 3.75H6.912a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859M12 3v8.25m0 0-3-3m3 3 3-3" />
+        </svg>
+
+
+
+
+
+      </div>
+
+
+    </div>
+  );
+};
+
+export default Navbar;
