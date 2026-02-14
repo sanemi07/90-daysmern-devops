912322d4ff6c19ba884767cfb6cdfea37b061fc3
sanemi07 <arjuntripathi1724@gmail.com>
2026-02-14 18:42:44 +0530

    feat: enhance ReveneuCard component with hover effects and update App layout

--- week8/tailwind/src/App.jsx
+++ week8/tailwind/src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
+
 import ReveneuCard from './components/ReveneuCard'
 import Navbar from './components/Navbar'
 import Overview from './components/Overview'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  
 
   return (
     <div >
       <Navbar></Navbar>
       <Overview></Overview>
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-2'>
+        <ReveneuCard   title={"Next Payout"} amount={"2312.23"} orders={"23"}></ReveneuCard>
+        <ReveneuCard title={"Amount Pending"} amount={"92,312.20"} orders={"13"}></ReveneuCard>
+        <ReveneuCard title={"Amount Processed"} amount={"23,92,31212.19"} orders={"0"} ></ReveneuCard>
+
+      </div>
     
    </div>
    
