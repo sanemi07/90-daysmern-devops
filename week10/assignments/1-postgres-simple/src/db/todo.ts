07b676d14a95c828a6c1c54e0cfce84e3cb74a1a
sanemi07 <arjuntripathi1724@gmail.com>
2026-02-21 11:14:29 +0530

    fix(db): use parameterized queries and return todo/user fields

--- week10/assignments/1-postgres-simple/src/db/todo.ts
+++ week10/assignments/1-postgres-simple/src/db/todo.ts
@@ -1,4 +1,4 @@
-import { client } from "..";
+import { client } from ".."
 /*
  * Function should insert a new todo for this user
  * Should return a todo object
@@ -11,7 +11,8 @@ import { client } from "..";
  */
 export async function createTodo(userId: number, title: string, description: string) {
     try {
-        const insertQuery=`INSERT INTO todos(user_id,title,description) VALUES('$1', '$2','$3')`
+        const insertQuery=`INSERT INTO todos(user_id,title,description) VALUES($1, $2,$3,false) 
+        RETURNING id,title,description,done `
         const values=[userId,title,description]
         const res=await client.query(insertQuery,values)
         return res.rows[0]
@@ -33,6 +34,17 @@ export async function createTodo(userId: number, title: string, description: str
  * }
  */
 export async function updateTodo(todoId: number) {
+    try {
+        const query=`UPDATE todos
+        SET done=true
+        WHERE id=$1
+        RETURNING id,title,description,done
+           `
+        const res=await client.query(query,[todoId])
+        return res.rows[0]
+    } catch (error) {
+         console.log(error)
+    }
 
 }
 
@@ -47,5 +59,19 @@ export async function updateTodo(todoId: number) {
  * }]
  */
 export async function getTodos(userId: number) {
+ try {
+        const query=`SELECT id,title,description,done  FROM  todos 
+        WHERE user_id=$1
+        ORDER BY id DESC
+        
+        
+        
+        
+        `
+        const res=await client.query(query)
+        return res.rows
+    } catch (error) {
+         console.log(error)
+    }
 
 }
--- week10/assignments/1-postgres-simple/src/db/user.ts
+++ week10/assignments/1-postgres-simple/src/db/user.ts
@@ -10,7 +10,14 @@ import { client } from "..";
  * }
  */
 export async function createUser(username: string, password: string, name: string) {
-    
+    try {
+        const insertQuery=` INSERT INTO users (username,password,name) VALUES($1,$2,$3) RETURNING username,password,name
+        `
+        const value=[username,password,name]
+        const res=await client.query(insertQuery,value)
+    } catch (error) {
+        
+    }
 }
 
 /*
@@ -22,5 +29,10 @@ export async function createUser(username: string, password: string, name: strin
  * }
  */
 export async function getUser(userId: number) {
-    
+    try {
+        const query=`SELECT username,password,name FROM users WHERE user_id=$1   RETURNING username,password,name`
+        const res=await client.query(query,[userId])
+    } catch (error) {
+        
+    }
 }
