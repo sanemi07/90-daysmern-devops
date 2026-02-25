a9ee44e69c3640e04cc6aab5acf01bec1dc8e0ee
sanemi07 <arjuntripathi1724@gmail.com>
2026-02-25 09:16:56 +0530

    register

--- week11/worker-app-1/src/Routes/userRoutes.ts
+++ week11/worker-app-1/src/Routes/userRoutes.ts
@@ -1,8 +1,9 @@
 import { Hono } from "hono";
-import primsa from "../db"
+import prisma from "../db"
 
 import zod from 'zod'
-import { Context } from "hono/jsx";
+import { Context } from "hono";
+import { jwt } from "jsonwebtoken";
 
 
 const signupSchema=zod.object({
@@ -23,14 +24,41 @@ const signinSchema=zod.object({
 const router=new Hono()
 
 
-router.post('/signup',async(c)=>{
+router.post('/signup',async(c:Context)=>{
  try{
-    const body=c.req.json()
+    const body:{
+      name:string
+      email:string
+      password:string
+    }=await c.req.json()
     const isValidBody=signupSchema.safeParse(body)
-    if(!isValidBody){
+    if(!isValidBody.success){
         return c.json({msg:"invalid inputs"},400)
     }
-    const user=await primsa.user.findFirst()
+    const user=await prisma.user.findFirst({where:{email:body.email}})
+    if(!user){
+      return c.json({msg:"email already exists"},401)
+    }
+    
+    const res=await prisma.user.create({
+      data:{
+         name:body.name,
+         email:body.email,
+         password:body.password
+      }
+    })
+    const userId=res.id
+    const token=await jwt.sign(userId,"bfusifgbeuif")
+    return c.json({
+      msg:"regstered saccesfully",
+      token,
+      user:{
+           userId: res.id,
+        username: res.name,
+        email: res.email,
+      }
+    })
+
 
  } catch (error) {
     return c.json(error,500)
